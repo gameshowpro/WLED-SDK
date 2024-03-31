f4e6ddd7af37a406cd407a0fd97056403572e9bd
Pieter <51260315+DevPieter@users.noreply.github.com>
2024-03-31 15:03:31 +0200

    📖 Add example to Program.cs

--- WLED-SDK/Program.cs
+++ WLED-SDK/Program.cs
@@ -1,25 +1,29 @@
-﻿using WLED_SDK.Builders;
-using WLED_SDK.Extensions.StateExtensions;
+﻿using WLED_SDK.Extensions.StateExtensions;
 using WLED_SDK.WledClients;
 
-Console.Clear();
+// Create a new WLED WebSocket client for the specified device (by IP or hostname)
+//  We use the 'using' statement to ensure the client is properly disposed of when we're done
+using var client = new WledWebsocketClient("[Your WLED Device IP or Hostname]");
 
-using var client = new WledWebsocketClient("ledstrip.local");
+// Connect to the WLED device
+//  This will wait until the WLED device sends it's initial state (can be disabled by passing false)
 await client.ConnectAsync();
 
 Console.WriteLine($"Connected to {client.Info!.Name} running WLED {client.Info.Version} ({client.Info.VersionId})");
 
-var segmentBuilder = new SegmentBuilder(client);
-segmentBuilder.AddSegment(189, 297, "Computer").Effect(80).Palette(0);
-segmentBuilder.AddSegment(0, 118, "Vensterbank").Effect(27).Palette(50);
-segmentBuilder.AddSegment(118, 189, "Extra").On(false);
+// Turn on the WLED device
+await client.TurnOnAsync();
 
-await client.SetSegmentsAsync(segmentBuilder);
+// Set the brightness to 50% (0-255)
+await client.SetBrightnessAsync(128);
+
+// Set an effect (e.g., Rainbow) (https://kno.wled.ge/features/effects/)
+await client.SetEffectAsync(9);
+
+// Set a palette (e.g., Default) (https://kno.wled.ge/features/palettes/)
+await client.SetPaletteAsync(0);
+
 await Task.Delay(1000);
 
-var builder = new PresetBuilder(5, "Dit is een test");
-builder.QuickLoadLabel("MOOI");
-
-await client.SavePresetAsync(builder);
-
-await Task.Delay(-1);
+// Stop the connection to the WLED device
+await client.StopAsync();
