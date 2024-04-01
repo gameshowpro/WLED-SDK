8b02dd77ea1835940b9296be4873365b453ca984
Pieter <51260315+DevPieter@users.noreply.github.com>
2024-04-01 16:34:41 +0200

    🎨 Add Color Extensions

--- WLED-SDK/Extensions/ColorHelperExtensions.cs
+++ WLED-SDK/Extensions/ColorHelperExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace WLED_SDK.Extensions;
 
-public static class ColorExtensions
+public static class ColorHelperExtensions
 {
     public static string ToHexString(this Color color, bool withHash = true) => $"{(withHash ? "#" : "")}{color.R:X2}{color.G:X2}{color.B:X2}";
     public static string ToHexString(this Color? color, bool withHash = true) => color is null ? "" : ((Color)color).ToHexString(withHash);
---
+++ WLED-SDK/Extensions/StateExtensions/ColorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using Newtonsoft.Json.Linq;
+using WLED_SDK.Utils;
+using WLED_SDK.WledClients;
+
+namespace WLED_SDK.Extensions.StateExtensions;
+
+public static class ColorExtensions
+{
+    public static async Task SetColorsAsync(this IWledClient client, Color? primary, Color? secondary, Color? tertiary, params int[]? segmentsIds)
+    {
+        var segments = SegmentUtils.ClampIds(client.GetStateOrThrow(), segmentsIds);
+        var colors = new JArray
+        {
+            primary?.ToHexString(false) ?? "",
+            secondary?.ToHexString(false) ?? "",
+            tertiary?.ToHexString(false) ?? ""
+        };
+
+        await client.SendJsonAsync(SegmentUtils.CreateSegmentsWith(new { col = colors }, segments));
+    }
+
+    public static async Task SetPrimaryColorAsync(this IWledClient client, Color? color, params int[]? segmentsIds)
+        => await client.SetColorsAsync(color, null, null, segmentsIds);
+
+    public static async Task SetSecondaryColorAsync(this IWledClient client, Color? color, params int[]? segmentsIds)
+        => await client.SetColorsAsync(null, color, null, segmentsIds);
+
+    public static async Task SetTertiaryColorAsync(this IWledClient client, Color? color, params int[]? segmentsIds)
+        => await client.SetColorsAsync(null, null, color, segmentsIds);
+}
